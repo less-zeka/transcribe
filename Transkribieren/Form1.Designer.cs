e7055dc637095dc878c4c73317fb014eadf2d90f
Less <info@open-source.tips>
2016-07-16 08:49:32 +0200

    Initial commit

---
+++ Transkribieren/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Transkribieren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.edtBackTime = new System.Windows.Forms.TextBox();
+            this.edtForeward = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPosition = new System.Windows.Forms.Label();
+            this.lblBackHelp = new System.Windows.Forms.Label();
+            this.lblForewardHelp = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.edtFocus = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.edtBackTimePause = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.edtFile = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(12, 48);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.play_Click);
+            // 
+            // edtBackTime
+            // 
+            this.edtBackTime.Location = new System.Drawing.Point(89, 88);
+            this.edtBackTime.Name = "edtBackTime";
+            this.edtBackTime.Size = new System.Drawing.Size(41, 20);
+            this.edtBackTime.TabIndex = 2;
+            this.edtBackTime.TabStop = false;
+            this.edtBackTime.Text = "5";
+            // 
+            // edtForeward
+            // 
+            this.edtForeward.Location = new System.Drawing.Point(89, 119);
+            this.edtForeward.Name = "edtForeward";
+            this.edtForeward.Size = new System.Drawing.Size(41, 20);
+            this.edtForeward.TabIndex = 3;
+            this.edtForeward.TabStop = false;
+            this.edtForeward.Text = "30";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "sec. back";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "sec foreward";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Position";
+            // 
+            // lblPosition
+            // 
+            this.lblPosition.AutoSize = true;
+            this.lblPosition.Location = new System.Drawing.Point(86, 197);
+            this.lblPosition.Name = "lblPosition";
+            this.lblPosition.Size = new System.Drawing.Size(22, 13);
+            this.lblPosition.TabIndex = 7;
+            this.lblPosition.Text = "--:--";
+            // 
+            // lblBackHelp
+            // 
+            this.lblBackHelp.AutoSize = true;
+            this.lblBackHelp.Location = new System.Drawing.Point(149, 88);
+            this.lblBackHelp.Name = "lblBackHelp";
+            this.lblBackHelp.Size = new System.Drawing.Size(99, 13);
+            this.lblBackHelp.TabIndex = 8;
+            this.lblBackHelp.Text = "ctrl + shift + \\u2190";
+            // 
+            // lblForewardHelp
+            // 
+            this.lblForewardHelp.AutoSize = true;
+            this.lblForewardHelp.Location = new System.Drawing.Point(149, 126);
+            this.lblForewardHelp.Name = "lblForewardHelp";
+            this.lblForewardHelp.Size = new System.Drawing.Size(114, 13);
+            this.lblForewardHelp.TabIndex = 9;
+            this.lblForewardHelp.Text = "ctrl + shift + right Arrow";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(103, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(96, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "ctrl + shift + space ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(86, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "set focus here";
+            // 
+            // edtFocus
+            // 
+            this.edtFocus.Location = new System.Drawing.Point(18, 244);
+            this.edtFocus.Name = "edtFocus";
+            this.edtFocus.Size = new System.Drawing.Size(41, 20);
+            this.edtFocus.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "sec. back when Pause";
+            // 
+            // edtBackTimePause
+            // 
+            this.edtBackTimePause.Location = new System.Drawing.Point(137, 154);
+            this.edtBackTimePause.Name = "edtBackTimePause";
+            this.edtBackTimePause.Size = new System.Drawing.Size(41, 20);
+            this.edtBackTimePause.TabIndex = 15;
+            this.edtBackTimePause.TabStop = false;
+            this.edtBackTimePause.Text = "3";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // edtFile
+            // 
+            this.edtFile.Location = new System.Drawing.Point(18, 13);
+            this.edtFile.Name = "edtFile";
+            this.edtFile.Size = new System.Drawing.Size(113, 20);
+            this.edtFile.TabIndex = 17;
+            this.edtFile.Text = "C:\\tmp\\Interview.wav";
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(144, 12);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(36, 23);
+            this.btnChooseFile.TabIndex = 18;
+            this.btnChooseFile.Text = "...";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 300);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.edtFile);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.edtBackTimePause);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.edtFocus);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lblForewardHelp);
+            this.Controls.Add(this.lblBackHelp);
+            this.Controls.Add(this.lblPosition);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.edtForeward);
+            this.Controls.Add(this.edtBackTime);
+            this.Controls.Add(this.btnPlay);
+            this.Name = "Form1";
+            this.Text = "Transcription - Less";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox edtBackTime;
+        private System.Windows.Forms.TextBox edtForeward;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPosition;
+        private System.Windows.Forms.Label lblBackHelp;
+        private System.Windows.Forms.Label lblForewardHelp;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox edtFocus;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox edtBackTimePause;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox edtFile;
+        private System.Windows.Forms.Button btnChooseFile;
+    }
+}
+
---
+++ Transkribieren/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace Transkribieren
+{
+    public partial class Form1 : Form
+    {
+
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private IWavePlayer _player;
+        private WaveFileReader _waveFile;
+        private string _soundLocation = @"C:\tmp\Interview.wav";
+        private readonly BackgroundWorker _worker;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            lblBackHelp.Text = "ctrl + shift + \u2190";
+            lblForewardHelp.Text = "ctrl + shift + \u2192";
+            SetupPlayer();
+
+            _worker = new BackgroundWorker();
+            _worker.DoWork += backgroundWorker_DoWork;
+            _worker.WorkerSupportsCancellation = true;
+
+            edtFocus.Focus();
+
+            // Modifier keys codes: Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+            // Compute the addition of each combination of the keys you want to be pressed
+            // ALT+CTRL = 1 + 2 = 3 , CTRL+SHIFT = 2 + 4 = 6...
+            RegisterHotKey(this.Handle, HotKeyPlayPause, 6, Keys.Space.GetHashCode());
+            RegisterHotKey(this.Handle, HotKeyBack, 6, Keys.Left.GetHashCode());
+            RegisterHotKey(this.Handle, HotKeyForeward, 6, Keys.Right.GetHashCode());
+        }
+
+        private void backgroundWorker_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            while (_player.PlaybackState == PlaybackState.Playing && !_worker.CancellationPending)
+            {
+                var postitionString = _waveFile.CurrentTime.ToString();
+                lblPosition.Invoke((MethodInvoker)delegate {
+                    lblPosition.Text = postitionString;
+                });
+            }
+        }
+
+
+        private void SetupPlayer()
+        {
+            var fileStream = File.OpenRead(_soundLocation);
+            _waveFile = new WaveFileReader(fileStream);
+            _player = new WaveOut();
+            _player.Init(_waveFile);
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+           PlayOrPause();
+        }
+
+        private void PlayOrPause()
+        {
+            if (_player.PlaybackState == PlaybackState.Stopped)
+            {
+                btnPlay.Text = "Stop";
+                _player.Play();
+                _worker.RunWorkerAsync();
+            }
+                
+            else if(_player.PlaybackState == PlaybackState.Paused)
+            {
+                int seconds;
+
+                if (int.TryParse(edtBackTimePause.Text, out seconds))
+                {
+                    btnPlay.Text = "Stop";
+                    GoBack(seconds);
+                    _worker.RunWorkerAsync();
+
+                }
+            }
+            else
+            {
+                btnPlay.Text = "Play";
+                _player.Pause();
+                _worker.CancelAsync();
+            }
+        } 
+
+        private void GoBack(int seconds)
+        {
+            var timeSpan = new TimeSpan(0, 0, 0, seconds, 0);
+            if (_waveFile.CurrentTime.Subtract(timeSpan) > TimeSpan.Zero)
+            {
+                _player.Pause();
+                _waveFile.CurrentTime = _waveFile.CurrentTime.Subtract(timeSpan);
+                _player.Play();
+            }
+        }
+
+        private void GoForeward()
+        {
+            _player.Pause();
+            int seconds;
+            int.TryParse(edtForeward.Text, out seconds);
+            _waveFile.CurrentTime = _waveFile.CurrentTime.Add(new TimeSpan(0, 0, 0, seconds, 0));
+            _player.Play();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                PlayOrPause();
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                int seconds;
+                int.TryParse(edtBackTime.Text, out seconds);
+                GoBack(seconds);
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                GoForeward();
+            }
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            var result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) 
+            {
+                edtFile.Text = openFileDialog1.FileName;
+                _soundLocation = openFileDialog1.FileName;
+                SetupPlayer();
+            }
+        }
+
+        const int HotKeyPlayPause = 1;
+        const int HotKeyBack = 2;
+        const int HotKeyForeward = 3;
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == HotKeyPlayPause)
+            {
+                PlayOrPause();
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == HotKeyBack)
+            {
+                int seconds;
+                int.TryParse(edtBackTime.Text, out seconds);
+                GoBack(seconds);
+            }
+            else if (m.Msg == 0x0312 && m.WParam.ToInt32() == HotKeyForeward)
+            {
+                GoForeward();
+            }
+            base.WndProc(ref m);
+        }
+
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Unregister hotkeys 
+            UnregisterHotKey(this.Handle, HotKeyPlayPause);
+            UnregisterHotKey(this.Handle, HotKeyBack);
+            UnregisterHotKey(this.Handle, HotKeyForeward);
+
+        }
+    }
+}
